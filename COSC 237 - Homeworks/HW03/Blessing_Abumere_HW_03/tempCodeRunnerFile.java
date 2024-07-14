08867bd55d636788abe7bb56e992cf642e4684db
Blessing <babumere04@gmail.com>
2024-07-14 01:05:10 -0400

    Delete tempCodeRunnerFile.java

--- COSC 237 - Homeworks/HW03/Blessing_Abumere_HW_03/tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-" - [head]");
-        System.out.println("Will copy in second list only names that don't start with J. List one destroyed.");
-        while (!list_1.isEmptyList()) {
-            item = list_1.front();
